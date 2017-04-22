0680b06bdcc3991ab52ab3094ff13ee796c7a12d
fady samir <fady samir>
2017-04-22 03:22:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express  =  require( 'express' );
+var multer   =  require( 'multer' );
+var path = require('path');
+
+var upload = multer( { dest: 'uploads/' } );
+var app = express();
+app.get( '/', function( req, res, next ){
+  return res.sendFile( __dirname +'/index.html');
+});
+
+app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
+  // Metadata about the uploaded file can now be found in req.file
+});
+
+app.listen( 8080, function() {
+  console.log( 'Express server listening on port 8080' );
+});
